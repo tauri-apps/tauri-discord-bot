4958d31994de210810cb9f407815e320d9b1376e
Laegel <valentin.chouaf@laposte.net>
2021-06-15 13:15:16 +0200

    feat: Added issue link generator

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,9 +1,11 @@
 const help = require('./help')
 const search = require('./search')
 const stats = require('./stats')
+const issue = require('./issue')
 
 module.exports = {
   help,
   search,
-  stats
+  stats,
+  issue
 }
--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,19 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js");
 
-const getEmbeddedMessage = (message, context, title = '') => {
+const getEmbeddedMessage = (message, context, title = "") => {
   const embed = new Discord.MessageEmbed()
-    .setColor('#ffffff')
+    .setColor("#ffffff")
     .setAuthor(title, `${context.icon}`)
     .setTimestamp()
-    .setFooter('API Latency is ' + `${Date.now() - message.createdTimestamp}` + ' ms • Created/Maintained by Tauri Team', message.author.displayAvatarURL)
+    .setFooter(
+      "API Latency is " +
+        `${Date.now() - message.createdTimestamp}` +
+        " ms • Created/Maintained by Tauri Team",
+      message.author.displayAvatarURL
+    );
 
-  return embed
-}
+  return embed;
+};
 
 /**
  * Gets distinct name of a user
@@ -19,14 +24,14 @@ const getEmbeddedMessage = (message, context, title = '') => {
  * @returns {string} distinct name of the user in form of 'UserName#1234' or 'DisplayName (UserName#1234)'
  */
 const getDistinctUsername = (user, member = null) => {
-  const distinctName = `${user.username}#${user.discriminator}`
-  const withDisplayName = member !== null
-  const displayName = withDisplayName ? member.displayName : ''
+  const distinctName = `${user.username}#${user.discriminator}`;
+  const withDisplayName = member !== null;
+  const displayName = withDisplayName ? member.displayName : "";
 
-  return withDisplayName ? `${displayName} (${distinctName})` : distinctName
-}
+  return withDisplayName ? `${displayName} (${distinctName})` : distinctName;
+};
 
-const USER_MENTION_REGEX = /^<@!?(\d+)>$/
+const USER_MENTION_REGEX = /^<@!?(\d+)>$/;
 
 /**
  * Gets user from a mention string
@@ -37,32 +42,74 @@ const USER_MENTION_REGEX = /^<@!?(\d+)>$/
  * @returns {string} distinct name of the user in form of 'UserName#1234' or 'DisplayName (UserName#1234)'
  */
 const getUserFromMention = (client, mention) => {
-  const matches = mention.match(USER_MENTION_REGEX)
-  if (!matches) return
+  const matches = mention.match(USER_MENTION_REGEX);
+  if (!matches) return;
 
-  const [, id] = matches
+  const [, id] = matches;
 
-  return client.users.cache.get(id)
-}
+  return client.users.cache.get(id);
+};
 
 const getDonationText = () => {
-  return '\n_Support Tauri development by [Donating](https://opencollective.com/tauri/)_\n'
-}
+  return "\n_Support Tauri development by [Donating](https://opencollective.com/tauri/)_\n";
+};
 
 const buildResultItem = (element) => {
-  let resultItemParts = []
+  let resultItemParts = [];
   for (let i = 0; i < 4; i++) {
-    if (element['hierarchy_lvl' + i]) {
-      resultItemParts.push(element['hierarchy_lvl' + i] + (element['hierarchy_lvl' + i].length > 60 ? element['hierarchy_lvl' + i].substring(0, 60) + '...' : ''));
+    if (element["hierarchy_lvl" + i]) {
+      resultItemParts.push(
+        element["hierarchy_lvl" + i] +
+          (element["hierarchy_lvl" + i].length > 60
+            ? element["hierarchy_lvl" + i].substring(0, 60) + "..."
+            : "")
+      );
     }
   }
-  return `\n[${resultItemParts.join(' > ')}](https://tauri.studio/en/docs/${element.url + (element.anchor ? '#' + element.anchor : '')})${element.content ? '\n> ' + element.content : ''}\n`
-}
+  return `\n[${resultItemParts.join(" > ")}](https://tauri.studio/en/docs/${
+    element.url + (element.anchor ? "#" + element.anchor : "")
+  })${element.content ? "\n> " + element.content : ""}\n`;
+};
+
+const argToFlag = (arg) => arg.replace("--", "");
+
+const hasFlagInParts = (arg, parts) =>
+  parts.find((part) => argToFlag(part) === arg);
+
+const getFlagIndexInParts = (arg, parts) =>
+  parts.findIndex((part) => argToFlag(part) === arg);
+
+const getFlagValueInParts = (arg, parts) => {
+  const flagIndex = getFlagIndexInParts(arg, parts);
+  return parts[flagIndex + 1];
+};
+
+const parseString = (input) => {
+  const delimiter = `["']`;
+  return input.match(new RegExp(`${delimiter}(.*)${delimiter}`))[1];
+};
+
+const sendReply = (title, content, message, context) => {
+  const embed = getEmbeddedMessage(
+    message,
+    context,
+    title
+  ).setDescription(content);
+
+  return context.isMentioned
+    ? message.channel.send({ embed })
+    : message.author.send({ embed });
+};
 
 module.exports = {
   getEmbeddedMessage,
   getDistinctUsername,
   getUserFromMention,
   buildResultItem,
-  getDonationText
-}
+  getDonationText,
+  hasFlagInParts,
+  getFlagIndexInParts,
+  getFlagValueInParts,
+  parseString,
+  sendReply,
+};
