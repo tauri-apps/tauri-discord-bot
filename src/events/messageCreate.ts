bf591fe6fe5d9570b4c0c5bbe5c03132a6c61bee
Simon Hyll <hyllsimon@gmail.com>
2022-07-23 16:33:25 +0200

    Added sending the users message to the thread and linking them there

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -4,6 +4,8 @@ import {
 	MessageActionRow,
 	MessageButton,
 	ThreadChannel,
+	ThreadManager,
+	TextChannel,
 } from 'discord.js';
 import { event } from 'jellycommands';
 import url_regex from 'url-regex-safe';
@@ -16,6 +18,7 @@ import { Url } from 'url';
 export default event({
 	name: 'messageCreate',
 	run: async ({ }, message) => {
+		// Rules for whether or not the message should be dealt with by the bot
 		const should_ignore =
 			message.author.bot ||
 			message.channel.type != 'GUILD_TEXT' ||
@@ -24,16 +27,44 @@ export default event({
 
 		// If a response is sent by a user in an auto thread channel
 		if (message.type === 'REPLY' && AUTO_THREAD_CHANNELS.includes(message.channelId) && !message.author.bot) {
-			// Reply to their message with instructions
-			const response = await message.reply(wrap_in_embed("Please send responses in the issues thread and not directly in this channel"))
+			// Get the channel as a TextChannel
+			const channel = message.channel as TextChannel
+			// Get the thread that the message refers to
+			let thread = channel.threads.cache.get(message.reference.messageId) as ThreadChannel
+			// If the thread wasn't found in the cache
+			if (!thread) {
+				// Get all active threads in the guild
+				const threads = (await message.guild.channels.fetchActiveThreads()).threads
+				// Get the thread
+				thread = threads.get(message.reference.messageId) as ThreadChannel
+			}
+			// Make msg available outside the if/else scope
+			let msg;
+			// If the thread was found
+			if (thread) {
+				// Create a message that mentions  the user
+				const usersMessage = wrap_in_embed(`Response by <@${message.author.id}>`) as MessageOptions
+				// Put their original message contents in the new message
+				usersMessage.content = message.content
+				// Send the message to the thread
+				await thread.send(usersMessage)
+				// Create response message to the user with a link to the thread
+				msg = wrap_in_embed(`Please send responses in the issues thread and not directly in this channel\n\n<#${thread.id}>`)
+			} else {
+				// Create response message to the user
+				msg = wrap_in_embed(`Please send responses in the issues thread and not directly in this channel`)
+			}
+			// Reply to the user
+			const response = await message.reply(msg)
 			// Delete their message
 			await message.delete()
 			// Delete the response 5 seconds later
 			setTimeout(() => {
 				response.delete()
-			}, 5000)
+			}, 10000)
 		}
 
+		// If the message should be ignored, return without further processing
 		if (should_ignore) return;
 
 		// Remove bloat from links and replace colons with semicolons
