6e10300c921e769008141ac64ce024bc7db0e9cf
Rigidity <me@rigidnetwork.com>
2024-05-24 13:09:24 -0400

    Delete everything

--- src/utils/promise.ts
+++
@@ -1,9 +0,0 @@
-export const undefined_on_error = async <T>(promise: Promise<T>) => {
-    try {
-        return await promise;
-    } catch {
-        return undefined;
-    }
-};
-
-export function no_op() {}
